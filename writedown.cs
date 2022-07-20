1db7456b6be3481032d3f2b4f50825359d6cd95b
SuYeon-Hwnag <109446753+SuYeon-Hwnag@users.noreply.github.com>
2022-07-20 10:54:19 +0900

    Create writedown.cs
    writedown

---
+++ writedown.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class writeline
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter String:");
+        string string1 = Console.ReadLine();
+        
+        Console.WriteLine("Enter String:");
+        string string2 = Console.ReadLine();
+
+        Console.Write(string1 + "\n");
+
+        Console.Write(string2 + "\n");
+
+        Console.WriteLine(string1);
+
+        Console.WriteLine(string2);
+
+        Console.WriteLine(string1 + "\n" + string2);
+
+        Console.WriteLine($"{string1}\n{string2}");
+
+    }
+}
