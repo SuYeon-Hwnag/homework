2c37bae054b5011351171c1243fb4305c725adb4
SuYeon-Hwnag <109446753+SuYeon-Hwnag@users.noreply.github.com>
2022-07-21 18:34:09 +0900

    ss
    ss

---
+++ stringint/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    
+namespace stringint
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Write string -> ");
+            string string1 = Console.ReadLine();
+            int int1 = int.Parse(string1);
+            float float1 = (float)int1;
+            string string2 = float1.ToString();
+
+        }
+    }
+}
