a3166e3cda1ff3301c164bc936e7da6e5b15d759
SuYeon-Hwnag <109446753+SuYeon-Hwnag@users.noreply.github.com>
2022-07-22 19:22:13 +0900

    ss
    ss

---
+++ plus/plus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plusminus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float float1;
+            float float2;
+
+            Console.WriteLine("Input float1: ");
+            float1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input float2: ");
+            float2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("+: {0}", float1 + float2);
+            Console.WriteLine("-: {0}", float1 - float2);
+            Console.WriteLine("*: {0}", float1 * float2);
+            Console.WriteLine("/: {0}", float1 / float2);
+            Console.WriteLine("%: {0}", float1 % float2);
+
+            Console.ReadKey();
+
+        }
+    }
+}
